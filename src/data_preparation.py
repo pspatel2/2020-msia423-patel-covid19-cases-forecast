9cddf58f733dab40c247948a4aa3ed94ae49d0a5
pspatel2 <pspatel.2011@gmail.com>
2020-05-31 15:09:27 -0500

    removed RDS arg and fixed missing aggregation in country dataframe preparation

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -86,10 +86,13 @@ def get_local_data(input_file_path):
             covid_data = json.load(f)
     except FileNotFoundError:
         logger.error("The file path you've specified does not exist. Verify the path is correct in the config.yml")
+        sys.exit(1)
     except ValueError as e:
         logger.error("The file specified does not seem to be of appropriate json form: {}".format(e))
+        sys.exit(1)
     except Exception as e:
         logger.error("Unexpected error with trying to read in raw data locally: {}:{}".format(type(e).__name__, e))
+        sys.exit(1)
 
     # convert data to a DataFrame
     covid_df = pd.DataFrame(covid_data)
@@ -111,8 +114,9 @@ def get_country_daily(df):
     Returns:
         df (pandas DataFrame): Reduced columns version of the input DataFrame
     '''
-    df.drop(columns=["Province","City","CityCode","Lat","Lon"])
-    return df
+    df.drop(columns=["Province","City","CityCode","Lat","Lon"],inplace=True)
+    country_df = df.groupby(['Country','Date']).sum().reset_index()
+    return country_df
 
 def get_global_daily(df):
     """
@@ -134,7 +138,6 @@ def run_data_preparation(args):
     Args:
         args: From argparse:
            - config (str): Path to yaml file with load_data as a top level key containing relevant configurations
-           - RDS (bool): the flag used to determine where the database schema will be created (RDS vs Local)
            - engine_string (str): sqlalchemy engine string argument can be entered
            - s3_flag (bool): the flag used to determine if the scripts will read/write via s3 or local
 
@@ -154,14 +157,13 @@ def run_data_preparation(args):
         df = get_local_data(**config['data_preparation']['get_local_data'])
     country_df = get_country_daily(df)
     global_df = get_global_daily(df)
-    helper.add_to_database(country_df,"country_covid_daily_cases",'replace',args.RDS,args.engine_string)
-    helper.add_to_database(global_df, "global_covid_daily_cases",'replace',args.RDS, args.engine_string)
+    helper.add_to_database(country_df,"country_covid_daily_cases",'replace',args.engine_string)
+    helper.add_to_database(global_df, "global_covid_daily_cases",'replace', args.engine_string)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Get covid data from s3 and prep for modeling')
     parser.add_argument('--config', '-c', default='config.yml', help='path to yaml file with configurations')
-    parser.add_argument("--RDS", dest='RDS',action='store_true',help="Use arg if if you want to create in RDS rather than in local database.")
     parser.add_argument("--engine_string", default=None, help="Optional engine string for db to add data to ")
     parser.add_argument("--s3", dest='s3_flag', action='store_true', help="Use arg if you want to save s3 rather than locally.")
     args = parser.parse_args()
